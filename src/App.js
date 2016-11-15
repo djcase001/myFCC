579fb6a72dae2bba0d6c5cd33b39432b8bf27311
Arnaud C <djcase001@gmail.com>
2016-11-15 10:07:33 -0500

    Removes unnecessary assets

--- src/App.js
+++ src/App.js
@@ -2,13 +2,60 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = { }
+  }
+
   componentWillMount() {
     
-  },
+     var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'alltime.json');
+    xhr.onreadystatechange = ()=>{
+      if(xhr.readyState === 4 && xhr.status === 200){
+        var informations = JSON.parse(xhr.responseText);
+        this.setState({
+          data : informations
+        });
+
+      }
+    }
+    xhr.send(null);    
+  }
+
   render() {
+
+    var temporaire = this.state.data;
+    var count = 0;
+      var campers = temporaire ? temporaire.map(camper => {
+        count++;
+        return (
+            <tr>
+              <td className="mdl-data-table__cell--non-numeric">{count}</td>
+              <td className="mdl-data-table__cell--non-numeric">{camper.username}</td>
+              <td className="mdl-data-table__cell--non-numeric">{camper.lastUpdate}</td>
+              <td className="mdl-data-table__cell--non-numeric">{camper.recent}</td>
+              <td className="mdl-data-table__cell--non-numeric">{camper.alltime}</td>
+            </tr>
+          );
+      }) : '';    
+    
     return (
       <div className="App">
-        My App
+        <table className="mdl-data-table mdl-js-data-table mdl-data-table--selectable mdl-shadow--2dp">
+          <thead>
+            <tr>
+              <th className="mdl-data-table__cell--non-numeric">#</th>
+              <th className="mdl-data-table__cell--non-numeric">username</th>
+              <th className="mdl-data-table__cell--non-numeric">last update</th>
+              <th>recent</th>
+              <th>alltime</th>
+            </tr>
+          </thead>
+          <tbody>
+              {campers}
+          </tbody>
+        </table>
       </div>
     );
   }
